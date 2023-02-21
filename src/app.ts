ce4c568fe1f2c4a9122438d2ab77da9ddfe4fb53
renangiaretta <renan.giaretta@hotmail.com>
2023-02-21 16:23:43 -0300

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express, { Application } from 'express'
 
 const app: Application = express()
 
-app.use(express.json)
+app.use(express.json())
 
-app.listen(3000, () => {
-    console.log('server is online')
-})
+export { app }
