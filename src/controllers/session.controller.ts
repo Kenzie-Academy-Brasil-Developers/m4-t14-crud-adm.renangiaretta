addc0bea3bab3a48a628bb8e5f51ddc7ca7bde80
renangiaretta <renan.giaretta@hotmail.com>
2023-02-27 22:28:21 -0300

    fix: comments

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express'
 import { loginService } from './../services/session/createSession.service'
 
 
-
 const loginController = async ( req: Request, res: Response ): Promise<Response> => {
     const data = await loginService(req.body.email, req.body.password)
     return res.status(201).json(data)
